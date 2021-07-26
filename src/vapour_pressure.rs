3edf5bc8c1b0b540392951fe27a758c59c41ba45
Jakub Lewandowski <jak.lewand@gmail.com>
2021-07-26 17:41:42 +0200

    clippy cleanup

--- src/vapour_pressure.rs
+++ src/vapour_pressure.rs
@@ -5,55 +5,63 @@ use crate::{constants::ZERO_CELSIUS, error_wrapper::InputError};
 ///Formula computing vapour pressure from air temperature and pressure.
 ///Most accurate in temperature range from 233K to 323K.
 ///
-///Valid temperature range: 232K - 324K\
-///Valid pressure range: 100Pa - 110000Pa\
 ///Derived by A. L. Buck (1981) [(doi: 10.1175/1520-0450(1981)020<1527:nefcvp>2.0.co;2)](https://doi.org/10.1175/1520-0450(1981)020%3C1527:NEFCVP%3E2.0.CO;2).
+///
+///# Errors
+///
+///Return [`InputError::OutOfRange`] when one of inputs is out of range.\
+///Valid temperature range: 232K - 324K\
+///Valid pressure range: 100Pa - 110000Pa
 pub fn buck1(temperature: f64, pressure: f64) -> Result<f64, InputError> {
     //validate inputs
-    if temperature < 232.0 || temperature > 324.0 {
+    if !(232.0..=324.0).contains(&temperature) {
         return Err(InputError::OutOfRange(String::from("temperature")));
     }
 
-    if pressure < 100.0 || pressure > 110000.0 {
+    if !(100.0..=110_000.0).contains(&pressure) {
         return Err(InputError::OutOfRange(String::from("pressure")));
     }
 
     let temperature = temperature - ZERO_CELSIUS; //convert to C
     let pressure = pressure / 100.0; //convert to hPa
 
-    let a = 6.1121;
-    let b = 18.729;
-    let c = 257.87;
-    let d = 227.3;
+    let lower_a = 6.1121;
+    let lower_b = 18.729;
+    let lower_c = 257.87;
+    let lower_d = 227.3;
 
-    let upper_a = 0.00072;
-    let upper_b = 0.0000032;
-    let upper_c = 0.00000000059;
+    let upper_a = 0.000_72;
+    let upper_b = 0.000_003_2;
+    let upper_c = 0.000_000_000_59;
 
-    let e = a * (((b - (temperature / d)) * temperature) / (temperature + c)).exp();
-    let f = 1.0 + upper_a + (pressure * (upper_b + (upper_c * temperature * temperature)));
+    let lower_e = lower_a * (((lower_b - (temperature / lower_d)) * temperature) / (temperature + lower_c)).exp();
+    let lower_f = 1.0 + upper_a + (pressure * (upper_b + (upper_c * temperature * temperature)));
 
-    Ok((e * f) * 100.0) //return in Pa
+    Ok((lower_e * lower_f) * 100.0) //return in Pa
 }
 
 ///Formula computing vapour pressure from air temperature over water. 
 ///Should be used for temperatures above 273K.
 ///
-///Valid temperature range: 273K - 353K\
 ///Derived by O. Tetens (1930).
+///
+///# Errors
+///
+///Return [`InputError::OutOfRange`] when input is out of range.\
+///Valid temperature range: 273K - 353K
 pub fn tetens1(temperature: f64) -> Result<f64, InputError> {
     //validate inputs
-    if temperature < 273.0 || temperature > 354.0 {
+    if !(273.0..=354.0).contains(&temperature) {
         return Err(InputError::OutOfRange(String::from("temperature")));
     }
 
     let temperature = temperature - 273.15; //convert to C
 
-    let a = 0.61078;
-    let b = 17.27;
-    let c = 237.3;
+    let lower_a = 0.61078;
+    let lower_b = 17.27;
+    let lower_c = 237.3;
 
-    let result = a * ((b * temperature) / (temperature + c)).exp();
+    let result = lower_a * ((lower_b * temperature) / (temperature + lower_c)).exp();
 
     Ok(result * 1000.0) //return in Pa
 }
