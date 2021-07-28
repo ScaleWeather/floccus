7a5657bbfa7ce5e73deb577af0e00b6f71b33541
Jakub Lewandowski <jak.lewand@gmail.com>
2021-07-28 11:33:21 +0200

    improved constants readability

--- src/constants.rs
+++ src/constants.rs
@@ -7,13 +7,13 @@ pub const ZERO_CELSIUS: f64 = 273.15;
 pub const G: f64 = 9.80665;
 
 ///Universal gas constant in J*K^-1*mol^-1
-pub const R: f64 = 8.31446261815324;
+pub const R: f64 = 8.314_462_618_153_24;
 
 ///Molar mass of dry air in kg*mol^-1 (ECMWF, 2020)
-pub const M_D: f64 = 0.0289644;
+pub const M_D: f64 = 0.028_964_4;
 
 ///Molar mass of water vapour in kg*mol^-1
-pub const M_V: f64 = 0.0180152833;
+pub const M_V: f64 = 0.018_015_283_3;
 
 ///Specific heat capacity of dry air at constant pressure in J*kg^-1*K^-1(ECMWF, 2020)
 pub const C_P: f64 = 1004.709;
@@ -28,7 +28,7 @@ pub const C_PV: f64 = 1846.1;
 pub const C_VV: f64 = 1384.575;
 
 ///Latent heat of vapourization of water in J*kg^1  (ECMWF, 2020)
-pub const L_V: f64 = 2500800.0;
+pub const L_V: f64 = 2_500_800.0;
 
 ///Ratio of molar masses of dry air and water vapour
 pub const EPSILON: f64 = M_V / M_D;
