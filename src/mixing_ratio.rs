fd4aafcc1cb875bc91cef456a0f196b82bc334ae
Jakub Lewandowski <jak.lewand@gmail.com>
2021-07-28 11:03:46 +0200

    added mixing ratio functions

--- src/mixing_ratio.rs
+++ src/mixing_ratio.rs
@@ -1,9 +1,21 @@
 //!Functions to calculate mixing ratio of fluids
 
-use crate::{constants::EPSILON, error_wrapper::InputError};
+use crate::{constants::EPSILON, error_wrapper::InputError, vapour_pressure};
 
-fn air1(pressure: f64, vapour_pressure: f64) -> Result<f64, InputError> {
+fn air_general1(pressure: f64, vapour_pressure: f64) -> Result<f64, InputError> {
     let result = EPSILON * (vapour_pressure / (pressure - vapour_pressure));
+    Ok(result)
+}
+
+fn air_performance1(temperature: f64, pressure: f64) -> Result<f64, InputError> {
+    let vapour_pressure = vapour_pressure::tetens1(temperature)?;
+    let result = air_general1(pressure, vapour_pressure)?;
+    Ok(result)
+}
+
+fn air_accuracy1(temperature: f64, pressure: f64) -> Result<f64, InputError> {
+    let vapour_pressure = vapour_pressure::buck1(temperature, pressure)?;
+    let result = air_general1(pressure, vapour_pressure)?;
     Ok(result)
 }
 
