3ae2b2f57a339417f46f5184be26887612fce3cc
Quba1 <22771850+Quba1@users.noreply.github.com>
2024-02-19 14:58:06 +0100

    theta w

--- src/wet_bulb_potential_temperature.rs
+++ src/wet_bulb_potential_temperature.rs
@@ -6,7 +6,7 @@ use crate::{
     errors::InputError,
 };
 
-#[cfg(feature="debug")]
+#[cfg(feature = "debug")]
 use floccus_proc::logerr;
 
 ///Formula for computing wet bulb potential temperature from equivalent potential temperature.
@@ -17,18 +17,29 @@ use floccus_proc::logerr;
 ///
 ///Returns [`InputError::OutOfRange`] when one of inputs is out of range.\
 ///Valid `temperature` range: 257K - 377K\
-#[cfg_attr(feature = "debug", logerr)]
 pub fn davies_jones1(equivalent_potential_temperature: Float) -> Result<Float, InputError> {
+    davies_jones1_validate(equivalent_potential_temperature)?;
+    Ok(davies_jones1_unchecked(equivalent_potential_temperature))
+}
+
+#[allow(missing_docs)]
+#[allow(clippy::missing_errors_doc)]
+#[cfg_attr(feature = "debug", logerr)]
+pub fn davies_jones1_validate(equivalent_potential_temperature: Float) -> Result<(), InputError> {
     if !(257.0..=377.0).contains(&equivalent_potential_temperature) {
         return Err(InputError::OutOfRange(String::from(
             "equivalent_potential_temperature",
         )));
     }
 
-    let lambda = C_P / R_D;
+    Ok(())
+}
 
+#[allow(missing_docs)]
+pub fn davies_jones1_unchecked(equivalent_potential_temperature: Float) -> Float {
+    let lambda = C_P / R_D;
     let result = 45.114 - 51.489 * (ZERO_CELSIUS / equivalent_potential_temperature).powf(lambda);
-    Ok(result + ZERO_CELSIUS)
+    result + ZERO_CELSIUS
 }
 
 #[cfg(test)]
