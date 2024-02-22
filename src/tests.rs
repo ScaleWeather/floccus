d0826b69392496a895300cd32d14341e30711748
Quba1 <22771850+Quba1@users.noreply.github.com>
2024-02-22 12:17:36 +0100

    rewrite vapour pressure

--- src/tests.rs
+++ src/tests.rs
@@ -1,5 +1,5 @@
 use crate::errors::InputError;
-use crate::formula::Formula2;
+use crate::formula::{Formula1, Formula2};
 use crate::quantities::ThermodynamicQuantity;
 use crate::Float;
 use float_cmp::assert_approx_eq;
@@ -89,6 +89,66 @@ pub fn test_with_2args<
     let result = F::compute(I1::new_si(arg1.def_val), I2::new_si(arg2.range[0] - 0.1)).unwrap_err();
     assert_eq!(result, expected);
     let result = F::compute(I1::new_si(arg1.def_val), I2::new_si(arg2.range[1] + 0.1)).unwrap_err();
+    assert_eq!(result, expected);
+}
+
+pub fn test_with_1arg<O: ThermodynamicQuantity, I1: ThermodynamicQuantity, F: Formula1<O, I1>>(
+    arg1: Argument,
+    expected_result: Float,
+) {
+    //the first promise of the crate is that returned value
+    //is calculated correctly
+    let result = F::compute(I1::new_si(arg1.def_val)).unwrap();
+    assert_approx_eq!(
+        Float,
+        result.get_si_value(),
+        expected_result,
+        epsilon = 0.01
+    );
+
+    // the second promise of the crate is to never return NaN or Inf
+    // here we check several edge cases for that
+    // the function can only return a finite number or InputError
+    // check for error implicit as Result<> ensures that if value
+    // is not Ok() then it is Err()
+    // here we don't care about error being correct
+    let results = vec![
+        F::compute(I1::new_si(arg1.def_val)),
+        F::compute(I1::new_si(-9999.0)),
+    ];
+
+    for result in results {
+        if let Ok(result) = result {
+            assert!(result.get_si_value().is_finite());
+        }
+    }
+
+    //the third promise of the crate is to always return finite f64
+    //if all inputs are within the range
+    //the only allowed error is InccorectArgumentsSet as it can occur
+    //for values within valid range
+    for arg1_itr in 0..=100 {
+        let arg1_tmp =
+            (((arg1.range[1] - arg1.range[0]) / 100.0) * arg1_itr as Float) + arg1.range[0];
+
+        let arg1_tmp = I1::new_si(arg1_tmp);
+
+        let result = F::compute(arg1_tmp);
+
+        match result {
+            Ok(r) => assert!(r.get_si_value().is_finite()),
+            Err(e) => assert!(
+                discriminant(&InputError::IncorrectArgumentSet(String::new())) == discriminant(&e)
+            ),
+        }
+    }
+
+    //the fourth promise of the crate is to return an error with
+    //erronous variable name when input is out of range
+    let expected = InputError::OutOfRange(String::from(arg1.name));
+    let result = F::compute(I1::new_si(arg1.range[0] - 0.1)).unwrap_err();
+    assert_eq!(result, expected);
+    let result = F::compute(I1::new_si(arg1.range[1] + 0.1)).unwrap_err();
     assert_eq!(result, expected);
 }
 
