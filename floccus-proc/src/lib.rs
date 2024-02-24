fde30eee12a1c763e7ccf83eb4081debcf8854de
Quba1 <22771850+Quba1@users.noreply.github.com>
2024-02-24 13:13:29 +0100

    actually remove proc from repo because vscode dont work

--- floccus-proc/src/lib.rs
+++
@@ -1,7 +0,0 @@
-//! Procedural macros for floccus.
-//! 
-//! This crate contains procedural attribute macros (currently only one) used by the
-//! [floccus](https://crates.io/crates/floccus). But potentially can be used in some
-//! other crates, although will never be adapted to work with any other crate.
-//! 
-//! Source code of this crate is a modified copy of [derive-name-macros](https://crates.io/crates/derive-name-macros).
