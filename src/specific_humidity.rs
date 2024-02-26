47e7c011ce4270013a93486249d0c0441b3456a4
Quba1 <22771850+Quba1@users.noreply.github.com>
2024-02-26 17:20:01 +0100

    specific humidity

--- src/specific_humidity.rs
+++ src/specific_humidity.rs
@@ -12,6 +12,8 @@ use crate::quantities::{
 };
 use crate::{constants::EPSILON, errors::InputError};
 
+type FormulaQuantity = SpecificHumidity;
+
 /// Formula for computing specific humidity from vapour pressure and pressure.
 /// Reverse function of [`vapour_pressure::general1`](crate::vapour_pressure::general1).
 /// This function is theoretical not empirical.
@@ -23,22 +25,14 @@ use crate::{constants::EPSILON, errors::InputError};
 /// Valid `pressure` range: 100Pa - 150000Pa
 pub struct Definition1;
 
-impl Formula2<SpecificHumidity, VapourPressure, AtmosphericPressure> for Definition1 {
+impl Formula2<FormulaQuantity, VapourPressure, AtmosphericPressure> for Definition1 {
     #[inline(always)]
     fn validate_inputs(
         vapour_pressure: VapourPressure,
         pressure: AtmosphericPressure,
     ) -> Result<(), InputError> {
-        let vapour_pressure_si = vapour_pressure.get_si_value();
-        let pressure_si = pressure.get_si_value();
-
-        if !(0.0..=50_000.0).contains(&vapour_pressure_si) {
-            return Err(InputError::OutOfRange(String::from("vapour_pressure")));
-        }
-
-        if !(100.0..=150_000.0).contains(&pressure_si) {
-            return Err(InputError::OutOfRange(String::from("pressure")));
-        }
+        vapour_pressure.check_range_si(0.0, 50_000.0)?;
+        pressure.check_range_si(100.0, 150_000.0)?;
 
         Ok(())
     }
@@ -57,24 +51,17 @@ impl Formula2<SpecificHumidity, VapourPressure, AtmosphericPressure> for Definit
 
 #[cfg(test)]
 mod tests {
-    use crate::tests::{test_with_2args, Argument};
+    use crate::tests::{test_with_2args, testing_traits::ReferenceAtmosphere, Argument};
 
     use super::*;
 
     #[test]
     fn definition1() {
-        test_with_2args::<SpecificHumidity, VapourPressure, AtmosphericPressure, Definition1>(
-            Argument {
-                name: "vapour_pressure",
-                def_val: 3000.0,
-                range: [0.0, 50_000.0],
-            },
-            Argument {
-                name: "pressure",
-                def_val: 101325.0,
-                range: [100.0, 150_000.0],
-            },
-            0.018623845512674677,
+        test_with_2args::<FormulaQuantity, VapourPressure, AtmosphericPressure, Definition1>(
+            Argument::new([0.0, 50_000.0]),
+            Argument::new([100.0, 150_000.0]),
+            ReferenceAtmosphere::Normal,
+            1e-12,
         );
     }
 }
